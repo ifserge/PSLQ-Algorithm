6edac11cbb1652c1a16a2f87c030ef3527dd6a06
sergeif <ifserge@gmail.com>
2015-07-22 22:58:13 +0300

    ! full implementation

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ This project using [Apache Spark](https://github.com/apache/spark) and [Breeze](
 ## How to run
 1. Start sbt. 
 2. Type run. REPL prompt will be appeared. 
-3. Type your set of real numbers (for example: 239.239239 3.1415926535 2.7818281828). 
+3. Type your set of real numbers (for example: -1 3.1415926535 . In this case the algorithm returns the good approximation of pi ). 
 4. PSLQ Algorithm will be applied and [integer relations](http://mathworld.wolfram.com/IntegerRelation.html) would be printed.
 5. Type "q:" without quotas for exit.
--- src/main/scala/PSLQAlgo.scala
+++ src/main/scala/PSLQAlgo.scala
@@ -40,12 +40,52 @@ object PSLQAlgo extends App {
           B(0 to (n-1), j-1) := B(0 to (n-1), j-1) + (B(0 to (n-1), i-1) :* t)
         }
         //step 2: iteration procedure
-        //for (it <- 1 to 10) {
+        val ys = DenseVector.tabulate(n){ i => y(i) }
+        while (min(ys.map(_.abs)) > 1e-6) {
           val gammaVector = DenseVector.tabulate(n-1){i => pow(gamma, i+1) * H(i,i)}
           val m = argmax(gammaVector) + 1
-          val ys = DenseVector.tabulate(n){ i => if (i == m-1) y(m) else if (i == m) y(m-1) else y(i) }
-        //}
-        
+          
+          //step 3: swap m-1 <-> m
+          ys := DenseVector.tabulate(n){ case(i) => if (i == m-1) ys(m) else if (i == m) ys(m-1) else ys(i) }
+          val Hm = DenseVector.tabulate(n-1){ i => H(m-1, i) }
+          H(m-1, 0 to (n-2)) := H(m, 0 to (n-2))
+          H(m, 0 to (n-2)) := Hm.t
+          val Am = DenseVector.tabulate(n){ i=>A(m-1, i) }
+          A(m-1, ::) := A(m, ::)
+          A(m, ::) := Am.t
+          val Bm = DenseVector.tabulate(n){i => B(i, m-1) }
+          B(::, m-1) := B(::, m)
+          B(::, m) := Bm
+
+          //step 4: not a corner case
+          if (m <= n-2) {
+            val t0 = sqrt(H(m-1,m-1) * H(m-1,m-1) + H(m-1,m)*H(m-1,m));
+            val t1 = H(m-1,m-1) / t0;
+            val t2 = H(m-1,m) / t0;
+            for (i <- m to n) {
+              val t3 = H(i-1,m-1)
+              val t4 = H(i-1,m)
+              H(i-1,m-1) = t1 * t3 + t2 * t4;
+              H(i-1,m) = -t2 * t3 + t1 * t4;
+            }
+          }
+
+          //step 5: reduction
+          for (i <- (m+1) to n) {
+            val js = Math.min(i-1, m+1)
+            for (j <- js to 1 by -1) {
+              val t = Math.floor(0.5 + H(i-1,j-1) / H(j-1,j-1));
+              ys(j-1) = ys(j-1) + t * ys(i-1)
+              H(i-1, 0 to (j-1)) := H(i-1, 0 to (j-1)) - (H(j-1, 0 to (j-1)) :* t)
+              A(i-1, 0 to (n-1)) := A(i-1, 0 to (n-1)) - (A(j-1, 0 to (n-1)) :* t)
+              B(0 to (n-1), j-1) := B(0 to (n-1), j-1) + (B(0 to (n-1), i-1) :* t)
+            }
+          }
+        }
+        //step 6: result
+        val resultColumn = argmin(ys.map(_.abs))
+        val result = B(::,resultColumn)
+        Console.println(result)
       }else break.break()
     } while (true)
   }
